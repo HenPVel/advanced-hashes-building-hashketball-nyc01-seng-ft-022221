97065721b27647c2b6b5e6de96a06073e796e314
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:31:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,33 +52,33 @@ hash={:Home=>{team_name:"Brooklyn Nets",
                              steals:2,
                              blocks:7,
                              slam_dunks:2},
-                             :Reggie_Evans=>
-                           {number:30,
-                             shoe:14,
+                             :Bismack_Biyombo=>
+                           {number:0,
+                             shoe:16,
                              points:12,
-                             rebounds:12,
-                             assists:12,
-                             steals:12,
-                             blocks:12,
-                             slam_dunks:7},
-                            :Brook_Lopez=>
-                          {number:11,
-                            shoe:17,
-                            points:17,
-                            rebounds:19,
-                            assists:10,
-                            steals:3,
+                             rebounds:4,
+                             assists:7,
+                             steals:22,
+                             blocks:15,
+                             slam_dunks:10},
+                            :DeSagna_Diop=>
+                          {number:2,
+                            shoe:14,
+                            points:24,
+                            rebounds:12,
+                            assists:12,
+                            steals:4,
+                            blocks:5,
+                            slam_dunks:5},
+                            :Ben_Gordon=>
+                          {number:8,
+                            shoe:15,
+                            points:33,
+                            rebounds:3,
+                            assists:2,
+                            steals:1,
                             blocks:1,
-                            slam_dunks:15},
-                            :Mason_Plumlee=>
-                          {number:1,
-                            shoe:19,
-                            points:26,
-                            rebounds:11,
-                            assists:6,
-                            steals:3,
-                            blocks:8,
-                            slam_dunks:5}]
+                            slam_dunks:0}]
                     }
      }
 
