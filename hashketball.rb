5ff4ee15a257b0528fb30e340c64ee1480f71d56
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:18:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,7 +21,24 @@ hash={:Home=>{team_name:"Brooklyn Nets",
                        steals:12,
                        blocks:12,
                        slam_dunks:7},
-                     ]
+                      Brook Lopez=>
+                    {number:11,
+                      shoe:17,
+                      points:17,
+                      rebounds:19,
+                      assists:10,
+                      steals:3,
+                      blocks:1,
+                      slam_dunks:15},
+                      Mason Plumlee=>
+                    {number:1,
+                      shoe:19,
+                      points:26,
+                      rebounds:11,
+                      assists:6,
+                      steals:3,
+                      blocks:8,
+                      slam_dunks:5}]
               }, #end of :home hash
 
       :Away=>{team_name:"Charlotte Hornets",
