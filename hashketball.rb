880c2bb7b57ef337558c08ccf9cb938be2f385e5
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 14:20:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,31 @@
 # Write your code here
 def game_hash
 
-hash={:Home=>{team_name:{},colors:{},players:[{}]},
-      :Away=>{team_name:{},colors:{},players:[{}]}
+hash={:Home=>{team_name:{},
+              colors:{},
+              players:[player_name:{},
+                       number:{},
+                       shoe:{},
+                       points:{},
+                       rebounds:{},
+                       assists:{},
+                       steals:{},
+                       blocks:{},
+                       slam_dunks:{}]
+              },
+
+      :Away=>{team_name:{},
+                    colors:{},
+                    players:[player_name:{},
+                             number:{},
+                             shoe:{},
+                             points:{},
+                             rebounds:{},
+                             assists:{},
+                             steals:{},
+                             blocks:{},
+                             slam_dunks:{}]
+            }
      }
 
 end
