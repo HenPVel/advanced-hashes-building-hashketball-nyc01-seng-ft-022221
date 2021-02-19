d18b502d974f9ce2ee4c92f8d00a106e5be0ec9b
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:50:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -41,7 +41,17 @@ hash={:home=>{team_name:"Brooklyn Nets",
                       assists:6,
                       steals:3,
                       blocks:8,
-                      slam_dunks:5}]
+                      slam_dunks:5},
+
+                    {player_name:"Jason Terry",
+                        number:31,
+                        shoe:15,
+                        points:19,
+                        rebounds:2,
+                        assists:2,
+                        steals:4,
+                        blocks:11,
+                        slam_dunks:1}]
               },#end of :home value
 
       :away=>{team_name:"Charlotte Hornets",
