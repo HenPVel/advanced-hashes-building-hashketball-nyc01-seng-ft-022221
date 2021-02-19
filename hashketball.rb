b4e4dc2890c7af5e43cdfe318a67828f9d3041f8
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:28:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,21 +39,48 @@ hash={:Home=>{team_name:"Brooklyn Nets",
                       steals:3,
                       blocks:8,
                       slam_dunks:5}]
-              }}#end of :home hash
+              },#end of :home value
 
-    #   :Away=>{team_name:"Charlotte Hornets",
-    #           colors:["Turquoise","Purple"],
-    #           players:[{player_name:{},
-    #                    number:{},
-    #                    shoe:{},
-    #                    points:{},
-    #                    rebounds:{},
-    #                    assists:{},
-    #                    steals:{},
-    #                    blocks:{},
-    #                    slam_dunks:{}}]
-    #         }
-    #  }
+      :Away=>{team_name:"Charlotte Hornets",
+                    colors:["Turquoise","Purple"],
+                    players:[:Jeff_Adrien=>
+                             {number:4,
+                             shoe:18,
+                             points:10,
+                             rebounds:1,
+                             assists:1,
+                             steals:2,
+                             blocks:7,
+                             slam_dunks:2},
+                             :Reggie_Evans=>
+                           {number:30,
+                             shoe:14,
+                             points:12,
+                             rebounds:12,
+                             assists:12,
+                             steals:12,
+                             blocks:12,
+                             slam_dunks:7},
+                            :Brook_Lopez=>
+                          {number:11,
+                            shoe:17,
+                            points:17,
+                            rebounds:19,
+                            assists:10,
+                            steals:3,
+                            blocks:1,
+                            slam_dunks:15},
+                            :Mason_Plumlee=>
+                          {number:1,
+                            shoe:19,
+                            points:26,
+                            rebounds:11,
+                            assists:6,
+                            steals:3,
+                            blocks:8,
+                            slam_dunks:5}]
+                    }
+     }
 
 end
 p game_hash
