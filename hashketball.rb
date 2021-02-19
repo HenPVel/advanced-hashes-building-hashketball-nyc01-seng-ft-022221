6cc6a9da9c89d2a1ac83f33ff46ca1a7e5e197c5
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:54:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,7 +65,7 @@ hash={:home=>{team_name:"Brooklyn Nets",
                              steals:2,
                              blocks:7,
                              slam_dunks:2},
-                             
+
                            {player_name:"Bismack Biyombo",
                              number:0,
                              shoe:16,
@@ -75,8 +75,10 @@ hash={:home=>{team_name:"Brooklyn Nets",
                              steals:22,
                              blocks:15,
                              slam_dunks:10},
-                            "DeSagna Diop"=>
-                          {number:2,
+
+
+                          {player_name:"DeSagna Diop",
+                            number:2,
                             shoe:14,
                             points:24,
                             rebounds:12,
@@ -84,8 +86,10 @@ hash={:home=>{team_name:"Brooklyn Nets",
                             steals:4,
                             blocks:5,
                             slam_dunks:5},
-                            "Ben Gordon"=>
-                          {number:8,
+
+                          
+                          {player_name:"Ben Gordon",
+                            number:8,
                             shoe:15,
                             points:33,
                             rebounds:3,
