6caf0fd5a52b915e70349a404deb80fe546956b8
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 14:08:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here
 def game_hash
 
-game_hash={:Home=>{team_name:{},colors:{},players:{}},:Away=>{team_name:{},colors:{},players:{}}}
+hash={:Home=>{team_name:{},colors:{},players:{}},:Away=>{team_name:{},colors:{},players:{}}}
 
 end
 game_hash
