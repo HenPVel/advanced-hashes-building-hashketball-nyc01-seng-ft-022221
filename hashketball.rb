029a72d6b17608db4eb727f4ab40195a2f9487ad
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:47:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,8 +12,9 @@ hash={:home=>{team_name:"Brooklyn Nets",
                        steals:3,
                        blocks:1,
                        slam_dunks:1},
-                       "Reggie Evans"=>
-                     {number:30,
+
+                     {player_name:"Reggie Evans",
+                       number:30,
                        shoe:14,
                        points:12,
                        rebounds:12,
@@ -21,8 +22,9 @@ hash={:home=>{team_name:"Brooklyn Nets",
                        steals:12,
                        blocks:12,
                        slam_dunks:7},
-                      "Brook Lopez"=>
-                    {number:11,
+
+                    {player_name:"Brook Lopez",
+                      number:11,
                       shoe:17,
                       points:17,
                       rebounds:19,
@@ -30,8 +32,9 @@ hash={:home=>{team_name:"Brooklyn Nets",
                       steals:3,
                       blocks:1,
                       slam_dunks:15},
-                      "Mason Plumlee"=>
-                    {number:1,
+                      
+                    {player_name:"Mason Plumlee"
+                      number:1,
                       shoe:19,
                       points:26,
                       rebounds:11,
