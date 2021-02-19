865a9ebdec07018e555e4064e704e81a045b5142
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 15:44:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ def game_hash
 
 hash={:home=>{team_name:"Brooklyn Nets",
               colors:["Black","White"],
-              players:["Alan Anderson"=>
-                       {number:0,
+              players:[{player_name:"Alan Anderson",
+                       number:0,
                        shoe:16,
                        points:22,
                        rebounds:12,
