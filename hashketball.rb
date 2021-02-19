bd2ace307b63edb8477b3111e84082d16a6542cd
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 14:43:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,15 +3,15 @@ def game_hash
 
 hash={:Home=>{team_name:"Brooklyn Nets",
               colors:[Black,White],
-              players:[{player_name:{Alan Anderson},
-                       number:{0},
-                       shoe:{16},
-                       points:{22},
-                       rebounds:{12},
-                       assists:{12},
-                       steals:{3},
-                       blocks:{1},
-                       slam_dunks:{1}}]
+              players:[{player_name:Alan Anderson,
+                       number:0,
+                       shoe:16,
+                       points:22,
+                       rebounds:12,
+                       assists:12,
+                       steals:3,
+                       blocks:1,
+                       slam_dunks:1}]
               }, #end of :home hash
 
       :Away=>{team_name:{},
